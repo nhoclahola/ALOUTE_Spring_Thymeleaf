8661e827db609dc40509f91541ca35c9da7244be
nhoclahola <nhoclahola@gmail.com>
2024-08-17 16:48:58 +0700

    Allow Async
    Maybe I will have some async functions in the future.

---
+++ src/main/java/com/nhoclahola/socialnetworkv1/configuration/AsyncConfig.java
@@ -0,0 +1,25 @@
+package com.nhoclahola.socialnetworkv1.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig
+{
+    @Bean(name = "taskExecutor")
+    public TaskExecutor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(25);
+        executor.setThreadNamePrefix("Async-");
+        executor.initialize();
+        return executor;
+    }
+}
